67f8c5c65f18ad7eb2865998fc7456eb03736202
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-11-24 12:12:48 +0000

    create project

---
+++ iDiet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDiet
+//
+//  Created by Ahmad Karkouty on 11/24/18.
+//  Copyright © 2018 Ahmad Karkouti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iDietTests/iDietTests.swift
@@ -0,0 +1,34 @@
+//
+//  iDietTests.swift
+//  iDietTests
+//
+//  Created by Ahmad Karkouty on 11/24/18.
+//  Copyright © 2018 Ahmad Karkouti. All rights reserved.
+//
+
+import XCTest
+@testable import iDiet
+
+class iDietTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
