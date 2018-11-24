58ff82192fd9f4a55e4ebd836d3fc8c2a37c54d3
FranckNdame <franck.mpouli@yahoo.com>
2018-11-24 21:09:12 +0000

    add navigation icon

--- iDiet/Controller/ProfileViewController.swift
+++ iDiet/Controller/ProfileViewController.swift
@@ -13,6 +13,10 @@ class ProfileViewController: UIViewController {
     
     // MARK: - IBOUTLETS
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var avatarView: UIView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var navigationUsernameLabel: UILabel!
     
     // MARK: - ATTRIBUTES
     var controllerColor: UIColor = UIColor(red: 0.23, green: 0.66, blue: 0.96, alpha: 1.0)
@@ -20,9 +24,10 @@ class ProfileViewController: UIViewController {
     // MARK: - CLASS METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        scrollView.delegate = self
         backgroundView.layer.cornerRadius = 0
         backgroundView.layer.masksToBounds = true
+        navigationUsernameLabel.alpha = 0
     }
     
     //MARK: - IBACTIONS
@@ -39,3 +44,24 @@ class ProfileViewController: UIViewController {
 
 // MARK: - ColoredView
 extension ProfileViewController: ColoredView {}
+
+extension ProfileViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y/150
+        
+//        print(1 - yOffset)
+        if yOffset >= 0 && yOffset <= 1 {
+            avatarView.alpha = 1 - yOffset - 0.2
+            usernameLabel.alpha = 1 - yOffset - 0.2
+            navigationUsernameLabel.alpha = yOffset - 0.2
+        } else if 1 - yOffset >= 0.9  {
+            avatarView.alpha = 1
+            usernameLabel.alpha = 1
+            navigationUsernameLabel.alpha = 0
+        } else {
+            avatarView.alpha = 0
+            usernameLabel.alpha = 0
+            navigationUsernameLabel.alpha = 1
+        }
+    }
+}
